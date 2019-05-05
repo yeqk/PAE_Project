00b9de006d37c8194c3bd0248eaab315a207a127
yeqk97 <yeqk97@gmail.com>
2019-05-05 19:53:13 +0200

    2G, 3G, 4G select options and map scale view added.

--- app/src/main/java/com/example/pae_project/HeatMap.java
+++ app/src/main/java/com/example/pae_project/HeatMap.java
@@ -1,16 +1,14 @@
 package com.example.pae_project;
 
-import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.style.expressions.Expression;
 import com.mapbox.mapboxsdk.style.layers.CircleLayer;
 import com.mapbox.mapboxsdk.style.layers.HeatmapLayer;
+import com.mapbox.mapboxsdk.style.layers.Layer;
 import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
@@ -27,6 +25,7 @@ import static com.mapbox.mapboxsdk.style.expressions.Expression.rgb;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.rgba;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.stop;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.zoom;
+import static com.mapbox.mapboxsdk.style.layers.Property.VISIBLE;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleColor;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleOpacity;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleRadius;
@@ -41,9 +40,16 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.heatmapWeight;
 public class HeatMap {
 
     private static final String ANT_2G_SOURCE_ID = "ANT_2Gs";
-    private static final String HEATMAP_LAYER_ID = "ANT_2Gs-heat";
-    private static final String HEATMAP_LAYER_SOURCE = "ANT_2Gs";
-    private static final String CIRCLE_LAYER_ID = "ANT_2Gs-circle";
+    private static final String ANT_3G_SOURCE_ID = "ANT_3Gs";
+    private static final String ANT_4G_SOURCE_ID = "ANT_4Gs";
+    private static final String HEATMAP_LAYER_2G_ID = "ANT_2Gs-heat";
+    private static final String HEATMAP_LAYER_3G_ID = "ANT_3Gs-heat";
+    private static final String HEATMAP_LAYER_4G_ID = "ANT_4Gs-heat";
+    private static final String CIRCLE_LAYER_2G_ID = "ANT_2Gs-circle";
+    private static final String CIRCLE_LAYER_3G_ID = "ANT_3Gs-circle";
+    private static final String CIRCLE_LAYER_4G_ID = "ANT_4Gs-circle";
+
+
 
     Context ctx;
 
@@ -55,19 +61,26 @@ public class HeatMap {
 
     private ValueAnimator attractionsColorAnimator;
 
-    public HeatMap(Context ctx) {
+    private Style style;
+
+    public HeatMap(Context ctx, Style st) {
         this.ctx = ctx;
+        style = st;
     }
 
     public void addANT_2GSource(@NonNull Style loadedMapStyle) {
-        GeoJsonSource hotelSource = new GeoJsonSource(ANT_2G_SOURCE_ID, loadJsonFromAsset("data.geojson"));
-        loadedMapStyle.addSource(hotelSource);
+        GeoJsonSource antenes2g = new GeoJsonSource(ANT_2G_SOURCE_ID, loadJsonFromAsset("data_2g.geojson"));
+        loadedMapStyle.addSource(antenes2g);
+        GeoJsonSource antenes3g = new GeoJsonSource(ANT_3G_SOURCE_ID, loadJsonFromAsset("data_3g.geojson"));
+        loadedMapStyle.addSource(antenes3g);
+        GeoJsonSource antenes4g = new GeoJsonSource(ANT_4G_SOURCE_ID, loadJsonFromAsset("data_4g.geojson"));
+        loadedMapStyle.addSource(antenes4g);
     }
 
     public void addHeatmapLayer(@NonNull Style loadedMapStyle) {
-        HeatmapLayer layer = new HeatmapLayer(HEATMAP_LAYER_ID, ANT_2G_SOURCE_ID);
+        HeatmapLayer layer = new HeatmapLayer(HEATMAP_LAYER_2G_ID, ANT_2G_SOURCE_ID);
         layer.setMaxZoom(9);
-        layer.setSourceLayer(HEATMAP_LAYER_SOURCE);
+        layer.setSourceLayer(ANT_2G_SOURCE_ID);
         layer.setProperties(
 
 // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
@@ -88,13 +101,6 @@ public class HeatMap {
 
 // Increase the heatmap color weight weight by zoom level
 // heatmap-intensity is a multiplier on top of heatmap-weight
-                heatmapIntensity(
-                        interpolate(
-                                linear(), zoom(),
-                                stop(0, 1),
-                                stop(9, 1)
-                        )
-                ),
 
 // Adjust the heatmap radius by zoom level
                 heatmapRadius(
@@ -116,10 +122,103 @@ public class HeatMap {
         );
 
         loadedMapStyle.addLayerAbove(layer, "waterway-label");
+
+        //-----------------------------------3G-------------------------------------
+        HeatmapLayer layer3g = new HeatmapLayer(HEATMAP_LAYER_3G_ID, ANT_3G_SOURCE_ID);
+        layer3g.setMaxZoom(9);
+        layer3g.setSourceLayer(ANT_3G_SOURCE_ID);
+        layer3g.setProperties(
+
+// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+// Begin color ramp at 0-stop with a 0-transparency color
+// to create a blur-like effect.
+                heatmapColor(
+                        interpolate(
+                                linear(), heatmapDensity(),
+                                literal(0), rgba(33, 102, 172, 0),
+                                literal(0.2), rgba(103, 169, 207, 0.5),
+                                literal(0.4), rgba(209, 229, 240, 0.5),
+                                literal(0.6), rgba(253, 219, 199, 0.5),
+                                literal(0.8), rgba(239, 138, 98, 0.5) ,
+                                literal(1), rgba(178, 24, 43 ,0.5)
+                        )
+                ),
+
+
+// Increase the heatmap color weight weight by zoom level
+// heatmap-intensity is a multiplier on top of heatmap-weight
+
+// Adjust the heatmap radius by zoom level
+                heatmapRadius(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(0, 2),
+                                stop(9, 15)
+                        )
+                ),
+
+// Transition from heatmap to circle layer by zoom level
+                heatmapOpacity(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(7, 0.7),
+                                stop(9, 0.7)
+                        )
+                )
+        );
+
+        loadedMapStyle.addLayerAbove(layer3g, HEATMAP_LAYER_2G_ID);
+
+        //------------------------------------4G-------------------------
+
+        HeatmapLayer layer4g = new HeatmapLayer(HEATMAP_LAYER_4G_ID, ANT_4G_SOURCE_ID);
+        layer4g.setMaxZoom(9);
+        layer4g.setSourceLayer(ANT_4G_SOURCE_ID);
+        layer4g.setProperties(
+
+// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+// Begin color ramp at 0-stop with a 0-transparency color
+// to create a blur-like effect.
+                heatmapColor(
+                        interpolate(
+                                linear(), heatmapDensity(),
+                                literal(0), rgba(33, 102, 172, 0),
+                                literal(0.2), rgba(103, 169, 207, 0.5),
+                                literal(0.4), rgba(209, 229, 240, 0.5),
+                                literal(0.6), rgba(253, 219, 199, 0.5),
+                                literal(0.8), rgba(239, 138, 98, 0.5) ,
+                                literal(1), rgba(178, 24, 43 ,0.5)
+                        )
+                ),
+
+
+// Increase the heatmap color weight weight by zoom level
+// heatmap-intensity is a multiplier on top of heatmap-weight
+
+// Adjust the heatmap radius by zoom level
+                heatmapRadius(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(0, 2),
+                                stop(9, 15)
+                        )
+                ),
+
+// Transition from heatmap to circle layer by zoom level
+                heatmapOpacity(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(7, 0.7),
+                                stop(9, 0.7)
+                        )
+                )
+        );
+
+        loadedMapStyle.addLayerAbove(layer4g, HEATMAP_LAYER_3G_ID);
     }
 
     public void addCircleLayer(@NonNull Style loadedMapStyle) {
-        CircleLayer circleLayer = new CircleLayer(CIRCLE_LAYER_ID, ANT_2G_SOURCE_ID);
+        CircleLayer circleLayer = new CircleLayer(CIRCLE_LAYER_2G_ID, ANT_2G_SOURCE_ID);
         circleLayer.setProperties(
 
 // Size circle radius by ANT_2G magnitude and zoom level
@@ -127,11 +226,9 @@ public class HeatMap {
                         4.0f
                 ),
 
-
-
 // Color circle by ANT_2G magnitude
                 circleColor(
-                        rgba(103, 169, 207, 0.5)
+                        rgba(0, 0, 255, 0.7)
                 ),
 
 // Transition from heatmap to circle layer by zoom level
@@ -146,9 +243,98 @@ public class HeatMap {
                 circleStrokeWidth(1.0f)
         );
 
-        loadedMapStyle.addLayerBelow(circleLayer, HEATMAP_LAYER_ID);
+        loadedMapStyle.addLayerBelow(circleLayer, HEATMAP_LAYER_2G_ID);
+
+        //------------------------------------3G---------------------------------
+        CircleLayer circleLayer3g = new CircleLayer(CIRCLE_LAYER_3G_ID, ANT_3G_SOURCE_ID);
+        circleLayer3g.setProperties(
+
+// Size circle radius by ANT_2G magnitude and zoom level
+                PropertyFactory.circleRadius(
+                        4.0f
+                ),
+
+// Color circle by ANT_2G magnitude
+                circleColor(
+                        rgba(0, 255, 0, 0.7)
+                ),
+
+// Transition from heatmap to circle layer by zoom level
+                circleOpacity(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(7, 0),
+                                stop(8, 1)
+                        )
+                ),
+                circleStrokeColor("white"),
+                circleStrokeWidth(1.0f)
+        );
+
+        loadedMapStyle.addLayerBelow(circleLayer3g, HEATMAP_LAYER_3G_ID);
+
+        //-----------------------------------------4G-----------------------
+        CircleLayer circleLayer4g = new CircleLayer(CIRCLE_LAYER_4G_ID, ANT_4G_SOURCE_ID);
+        circleLayer4g.setProperties(
+
+// Size circle radius by ANT_2G magnitude and zoom level
+                PropertyFactory.circleRadius(
+                        4.0f
+                ),
+
+// Color circle by ANT_2G magnitude
+                circleColor(
+                        rgba(255, 0, 0, 0.7)
+                ),
+
+// Transition from heatmap to circle layer by zoom level
+                circleOpacity(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(7, 0),
+                                stop(8, 1)
+                        )
+                ),
+                circleStrokeColor("white"),
+                circleStrokeWidth(1.0f)
+        );
+
+        loadedMapStyle.addLayerBelow(circleLayer4g, HEATMAP_LAYER_4G_ID);
     }
 
+    private void setLayerVisible(String layerId,@NonNull Style loadedMapStyle) {
+        Layer layer = loadedMapStyle.getLayer(layerId);
+        if (layer == null) {
+            return;
+        }
+        if (VISIBLE.equals(layer.getVisibility().getValue())) {
+// Layer is visible
+            layer.setProperties(
+                    PropertyFactory.visibility(Property.NONE)
+            );
+        } else {
+// Layer isn't visible
+            layer.setProperties(
+                    PropertyFactory.visibility(VISIBLE)
+            );
+        }
+    }
+
+    public  void set2GVisible(){
+        setLayerVisible(CIRCLE_LAYER_2G_ID,style);
+        setLayerVisible(HEATMAP_LAYER_2G_ID,style);
+
+    }
+
+    public void set3GVisible() {
+        setLayerVisible(CIRCLE_LAYER_3G_ID,style);
+        setLayerVisible(HEATMAP_LAYER_3G_ID,style);
+    }
+
+    public void set4GVisible() {
+        setLayerVisible(CIRCLE_LAYER_4G_ID,style);
+        setLayerVisible(HEATMAP_LAYER_4G_ID,style);
+    }
 
     private String loadJsonFromAsset(String filename) {
 // Using this method to load in GeoJSON files from the assets folder.
